3f3323a62ee2782f66f0cc8c99f7753dafc4ca47
003random <003random@protonmail.com>
2017-11-25 02:30:31 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+    home_dir=$(pwd)
+
+    dependencies_dir="dependencies"  
+
+    [ -d $output_dir ] || mkdir $output_dir;
+    [ -d $dependencies_dir ] || mkdir $dependencies_dir; git clone https://github.com/aboul3la/Sublist3r.git $dependencies_dir/sublister; git clone https://github.com/wpscanteam/wpscan.git $dependencies_dir/wpscan; git clone https://github.com/jobertabma/relative-url-extractor.git $dependencies_dir/relative-url-extractor; git clone https://github.com/maaaaz/webscreenshot.git $dependencies_dir/webscreenshot; chmod -R 777 $home_dir/$tools_dir; 
+
