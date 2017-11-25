2cced63f4f584ddc19d3a40c3f733cf752f78ee0
003random <003random@protonmail.com>
2017-11-25 02:29:12 +0100

    Removed the create dir and dependencies

--- recon.sh
+++ recon.sh
@@ -6,10 +6,6 @@
     dependencies_dir="dependencies"  
     screenshots_dir="$output_dir/$@/screenshots"  
 
-    [ -d $output_dir ] || mkdir $output_dir;
-    [ -d $dependencies_dir ] || mkdir $dependencies_dir; git clone https://github.com/aboul3la/Sublist3r.git $dependencies_dir/sublister; git clone https://github.com/wpscanteam/wpscan.git $dependencies_dir/wpscan; git clone https://github.com/jobertabma/relative-url-extractor.git $dependencies_dir/relative-url-extractor; git clone https://github.com/maaaaz/webscreenshot.git $dependencies_dir/webscreenshot; chmod -R 777 $home_dir/$tools_dir; 
-
-
 
     all_domains_file="$output_dir/$@/domains-all.txt"
     domains_file="$output_dir/$@/domains.txt"
