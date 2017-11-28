2fe7eaa8bc0244d4d390d53fe2bf7ce983e19b79
003random <003random@protonmail.com>
2017-11-28 18:23:48 +0100

    added nmap in the install

--- install.sh
+++ install.sh
@@ -8,5 +8,11 @@
     git clone https://github.com/wpscanteam/wpscan.git $dependencies_dir/wpscan; 
     git clone https://github.com/jobertabma/relative-url-extractor.git $dependencies_dir/relative-url-extractor; 
     git clone https://github.com/maaaaz/webscreenshot.git $dependencies_dir/webscreenshot; 
+    git clone https://github.com/nmap/nmap.git $dependencies_dir/nmap
+    cd $dependencies_dir/nmap;
+    ./configure;
+    make;
+    make install;
+    cd ../../;
     chmod -R 777 $home_dir/$tools_dir; 
 
