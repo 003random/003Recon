fa05c3c41f03c7ec1d689e0a6329811aab272fab
Abhinash Jain <labor.omnia.vincit18@gmail.com>
2017-12-06 21:13:25 +0530

    fix open_redirect 'r' error

--- tools/open_redirect.py
+++ tools/open_redirect.py
@@ -24,22 +24,22 @@ def start():
             
                 try:
                     r = requests.head(url, allow_redirects=True, timeout=5)
-                except:
-                    print "[-]Error on " + url
 
-                if r.history:  
-                    if r.url == "https://example.com":
-                        print "[+]"+url
-                        if is_closed:
-                            file = open(output_file,"w+")
-                        is_closed = False
-                        file.write(url + "\n")
+                    if r.history:  
+                        if r.url == "https://example.com":
+                            print "[+]"+url
+                            if is_closed:
+                                file = open(output_file,"w+")
+                            is_closed = False
+                            file.write(url + "\n")
+                        else:
+                            print "[-]"+url
                     else:
                         print "[-]"+url
-                else:
-                    print "[-]"+url
-        else:
-            print "[-]Domain is invalid"
+                except:
+                    print "[-]Error on " + url
+            else:
+                print "[-]Domain is invalid"
 
     if is_closed == False:
         file.close()
