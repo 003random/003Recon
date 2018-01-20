d0d365a8528814d6395f04e60baacb6da74ac96c
003random <003random@protonmail.com>
2018-01-20 21:49:47 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ dependencies_dir="dependencies"
 output_dir="output"
 
 mkdir $output_dir;
-mkdir -p $dependencies_dir/phantomjs; 
+#mkdir -p $dependencies_dir/phantomjs; 
    
 # Install sublis3r   
 git clone https://github.com/aboul3la/Sublist3r.git $dependencies_dir/sublister; 
@@ -19,29 +19,14 @@ git clone https://github.com/jobertabma/relative-url-extractor.git $dependencies
 git clone https://github.com/maaaaz/webscreenshot.git $dependencies_dir/webscreenshot; 
 
 # Install PhantomJS
-wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2 -O $dependencies_dir/phantomjs/phantomjs-2.1.1-linux-x86_64.tar.bz2
-tar xvjf $dependencies_dir/phantomjs/phantomjs-2.1.1-linux-x86_64.tar.bz2 -C /usr/local/share/
-ln -s /usr/local/share/phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/local/bin/
+#wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2 -O $dependencies_dir/phantomjs/phantomjs-2.1.1-linux-x86_64.tar.bz2
+#tar xvjf $dependencies_dir/phantomjs/phantomjs-2.1.1-linux-x86_64.tar.bz2 -C /usr/local/share/
+#ln -s /usr/local/share/phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/local/bin/
     
--# Install Nmap
-+git clone https://github.com/nmap/nmap.git $dependencies_dir/nmap
--# Checks if nmap is already installed on the system,
-+cd $dependencies_dir/nmap;
--# If installed, this creates a symbolic link of existing nmap to the dependencies directory.
-+./configure;
--# If nmap isn't installed then it will clone it from github and gets installed.
-+make;
--if [ -x "$(command -v nmap)" ]; then
-+make install;
--  mkdir nmap;
-+cd ../../;
--  ln -s "$(command -v nmap)" nmap/;
--  cd ../;
--else
--  git clone https://github.com/nmap/nmap.git $dependencies_dir/nmap
--  cd $dependencies_dir/nmap;
--  ./configure;
--  make;
--  make install;
--  cd ../../;
--fi
+ # Install Nmap
+git clone https://github.com/nmap/nmap.git $dependencies_dir/nmap
+cd $dependencies_dir/nmap;
+./configure;
+make;
+make install;
+cd ../../;
