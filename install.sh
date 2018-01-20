119e374313003d9e56da5024f937d1be9d96e794
003random <003random@protonmail.com>
2018-01-20 21:41:56 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,19 +23,9 @@ wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_6
 tar xvjf $dependencies_dir/phantomjs/phantomjs-2.1.1-linux-x86_64.tar.bz2 -C /usr/local/share/
 ln -s /usr/local/share/phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/local/bin/
     
-# Install Nmap
-# Checks if nmap is already installed on the system,
-# If installed, this creates a symbolic link of existing nmap to the dependencies directory.
-# If nmap isn't installed then it will clone it from github and gets installed.
-if [ -x "$(command -v nmap)" ]; then
-  mkdir nmap;
-  ln -s "$(command -v nmap)" nmap/;
-  cd ../;
-else
-  git clone https://github.com/nmap/nmap.git $dependencies_dir/nmap
-  cd $dependencies_dir/nmap;
-  ./configure;
-  make;
-  make install;
-  cd ../../;
-fi
+git clone https://github.com/nmap/nmap.git $dependencies_dir/nmap
+cd $dependencies_dir/nmap;
+./configure;
+make;
+make install;
+cd ../../;
