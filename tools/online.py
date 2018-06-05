e3f811730e31731743675272b3228715e7dd79ae
003random <003random@protonmail.com>
2018-06-05 18:24:33 +0000

    Update online.py

--- tools/online.py
+++ tools/online.py
@@ -1,26 +1,6 @@
 #!/usr/bin/python
 
-import httplib
-import socket
-import re
-import sys
-
-def online(host):
-    try:
-        socket.gethostbyname(host)
-    except socket.gaierror:
-        return False
-    else:
-        return True
-
-def available(host, path="/"):
-    try:
-        conn = httplib.HTTPConnection(host, timeout=5)
-        conn.request("HEAD", path)
-        if re.match("^[23]\d\d$", str(conn.getresponse().status)):
-            return True
-    except StandardError:
-        return False
+import sys, requests
 
 input_file = sys.argv[1]
 output_file = sys.argv[2]
@@ -32,13 +12,26 @@ domains = input_file_open.readlines()
 
 print("\n-- Writing online hosts in "+input_file+" to "+output_file+" --\n")
 
+
+def available(domain):
+        try:
+                r = requests.get(domain, timeout=3)
+                return True
+        except:
+                return False
+
 for domain in domains:
-    domain = domain.strip()
-    if online(domain) == True and available(domain) == True:
-        print("[+]"+domain.strip())
-        output_file_open.write(domain+"\n")
-    else:
-        print("[-]"+domain)
+        domain = domain.strip()
+
+        http = available("http://" + domain)
+        https = available("https://" + domain)
+
+        if http == True or https == True:
+                print("[+]" + domain.strip())
+                output_file_open.write(domain+"\n")
+        else:
+                print("[-]" + domain.strip())
+
 
 input_file_open.close()
 output_file_open.close()
